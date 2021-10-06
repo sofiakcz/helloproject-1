9450f7dc0bb8e8455c618d377c3e07cc66da14af
Edgar Neukirchner <nr@bulme.at>
2021-10-06 10:37:53 +0200

    Interactivity

--- hello.cpp
+++ hello.cpp
@@ -1,9 +1,15 @@
 // mein hello-Programm
 #include <iostream>
+using namespace std;
 
 int main()
 {
-	std::cout << "Hallo 4BHEL!" << std::endl;
+	string name;
+	cout << "Hallo!" << endl;
+	cout << "Dein Name: ";
+	cin >> name;
+	cout << "Hallo " << name; 
+	cout << " aus der 4BHEl!" << endl;
 	return 0;
 }
 
